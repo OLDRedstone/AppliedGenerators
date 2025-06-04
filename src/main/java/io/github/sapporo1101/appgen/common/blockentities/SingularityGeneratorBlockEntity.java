9655359c185d53e3f95ab82ae7365b37549ac8d5
sapporo1101 <71534849+sapporo1101@users.noreply.github.com>
2025-06-04 18:22:15 +0900

    remake singularity generator generation system

--- src/main/java/io/github/sapporo1101/appgen/common/blockentities/SingularityGeneratorBlockEntity.java
+++ src/main/java/io/github/sapporo1101/appgen/common/blockentities/SingularityGeneratorBlockEntity.java
@@ -1,18 +1,15 @@
 package io.github.sapporo1101.appgen.common.blockentities;
 
-import appeng.api.config.Actionable;
-import appeng.api.networking.IGrid;
 import appeng.api.networking.IGridNode;
-import appeng.api.networking.energy.IEnergyService;
 import appeng.api.networking.ticking.IGridTickable;
 import appeng.api.networking.ticking.TickRateModulation;
 import appeng.api.networking.ticking.TickingRequest;
 import appeng.api.stacks.AEItemKey;
+import appeng.api.stacks.AEKey;
 import appeng.api.stacks.AEKeyType;
 import appeng.api.stacks.GenericStack;
 import appeng.api.util.AECableType;
 import appeng.blockentity.grid.AENetworkedBlockEntity;
-import appeng.core.AEConfig;
 import appeng.core.definitions.AEItems;
 import appeng.core.settings.TickRates;
 import appeng.helpers.externalstorage.GenericStackInv;
@@ -30,7 +27,6 @@ import net.minecraft.core.Direction;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.RegistryFriendlyByteBuf;
-import net.minecraft.util.Mth;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
@@ -42,39 +38,23 @@ import java.util.Set;
 
 public class SingularityGeneratorBlockEntity extends AENetworkedBlockEntity implements IGridTickable, IGenericInvHost {
     private final GenericStackInv inv;
-    private double currentFuelTicksPerTick;
-    private double remainingFuelTicks;
-    private double fuelItemFuelTicks;
-    private double minFuelTicksPerTick;
-    private double maxFuelTicksPerTick;
-    private double initialFuelTicksPerTick;
+    private double generatableEnergy;
     public boolean isOn;
-    private final double minEnergyRate;
-    private final double baseMaxEnergyRate;
-    private final double initialEnergyRate;
 
     public SingularityGeneratorBlockEntity(BlockPos pos, BlockState blockState) {
         super(GlodUtil.getTileType(SingularityGeneratorBlockEntity.class, SingularityGeneratorBlockEntity::new, AGSingletons.SINGULARITY_GENERATOR), pos, blockState);
         this.inv = new CustomStackInv(
-                Map.of(0, Set.of(AEItemKey.of(AEItems.SINGULARITY)), 1, Set.of(FluxKey.of(EnergyType.FE))),
+                Map.of(0, Set.of(this.getFuelKey()), 1, Set.of(this.getEnergyKey())),
                 Map.of(0, CustomIOFilter.INSERT_ONLY, 1, CustomIOFilter.EXTRACT_ONLY),
                 this::setChanged, GenericStackInv.Mode.STORAGE, 2
         );
         this.inv.setCapacity(AEKeyType.fluids(), 0);
         if (ExternalTypes.GAS != null) this.inv.setCapacity(ExternalTypes.GAS, 0);
         if (ExternalTypes.MANA != null) this.inv.setCapacity(ExternalTypes.MANA, 0);
-        if (ExternalTypes.FLUX != null) this.inv.setCapacity(ExternalTypes.FLUX, 1073741824);
+        if (ExternalTypes.FLUX != null) this.inv.setCapacity(ExternalTypes.FLUX, this.getEnergyCapacity());
         if (ExternalTypes.SOURCE != null) this.inv.setCapacity(ExternalTypes.SOURCE, 0);
-        this.remainingFuelTicks = 0.0F;
-        this.fuelItemFuelTicks = 0.0F;
-        this.getMainNode().setIdlePowerUsage(0.0F).setFlags().addService(IGridTickable.class, this);
-        this.minEnergyRate = AEConfig.instance().getVibrationChamberMinEnergyPerGameTick();
-        this.baseMaxEnergyRate = AEConfig.instance().getVibrationChamberMaxEnergyPerGameTick();
-        this.initialEnergyRate = Mth.clamp(AEConfig.instance().getVibrationChamberBaseEnergyPerFuelTick(), this.minEnergyRate, this.baseMaxEnergyRate);
-        this.minFuelTicksPerTick = this.minEnergyRate / this.getEnergyPerFuelTick();
-        this.maxFuelTicksPerTick = this.baseMaxEnergyRate / this.getEnergyPerFuelTick();
-        this.initialFuelTicksPerTick = this.initialEnergyRate / this.getEnergyPerFuelTick();
-        this.currentFuelTicksPerTick = this.initialFuelTicksPerTick;
+        this.generatableEnergy = 0F;
+        this.getMainNode().setIdlePowerUsage(0F).setFlags().addService(IGridTickable.class, this);
     }
 
     public AECableType getCableConnectionType(Direction dir) {
@@ -90,30 +70,25 @@ public class SingularityGeneratorBlockEntity extends AENetworkedBlockEntity impl
 
     protected void writeToStream(RegistryFriendlyByteBuf data) {
         super.writeToStream(data);
-        this.isOn = this.getRemainingFuelTicks() > (double) 0.0F;
+        this.isOn = this.getGeneratableEnergy() > 0;
         data.writeBoolean(this.isOn);
     }
 
     public void saveAdditional(CompoundTag data, HolderLookup.Provider registries) {
         super.saveAdditional(data, registries);
         this.inv.writeToChildTag(data, "inv", registries);
-        data.putDouble("burnTime", this.getRemainingFuelTicks());
-        data.putDouble("maxBurnTime", this.getFuelItemFuelTicks());
-        int speed = (int) (this.currentFuelTicksPerTick * (double) 100.0F / this.maxFuelTicksPerTick);
-        data.putInt("burnSpeed", speed);
+        data.putDouble("burnTime", this.getGeneratableEnergy());
     }
 
     public void loadTag(CompoundTag data, HolderLookup.Provider registries) {
         super.loadTag(data, registries);
         this.inv.readFromChildTag(data, "inv", registries);
-        this.setRemainingFuelTicks(data.getDouble("burnTime"));
-        this.setFuelItemFuelTicks(data.getDouble("maxBurnTime"));
-        this.setCurrentFuelTicksPerTick((double) data.getInt("burnSpeed") * this.maxFuelTicksPerTick / (double) 100.0F);
+        this.setGeneratableEnergy(data.getDouble("burnTime"));
     }
 
     @Override
     public void setChanged() {
-        if (this.getRemainingFuelTicks() <= (double) 0.0F && this.canEatFuel()) {
+        if (this.getGeneratableEnergy() <= 0 && this.canEatFuel()) {
             this.getMainNode().ifPresent((grid, node) -> grid.getTickManager().wakeDevice(node));
         }
         super.setChanged();
@@ -121,11 +96,9 @@ public class SingularityGeneratorBlockEntity extends AENetworkedBlockEntity impl
 
     private boolean canEatFuel() {
         GenericStack is = this.inv.getStack(0);
-        if (is != null) {
-            int newBurnTime = 100;
-            return newBurnTime > 0 && is.amount() > 0;
+        if (is != null && is.what() != null && is.what().equals(this.getFuelKey())) {
+            return is.amount() > 0;
         }
-
         return false;
     }
 
@@ -145,72 +118,41 @@ public class SingularityGeneratorBlockEntity extends AENetworkedBlockEntity impl
     }
 
     public TickingRequest getTickingRequest(IGridNode node) {
-        System.out.println("Vibration Chamber Ticking Request: " + this.getRemainingFuelTicks() + " fuel ticks remaining, " + this.isOn);
-        if (this.getRemainingFuelTicks() <= (double) 0.0F) {
-            this.eatFuel();
+        System.out.println("Singularity Generator Ticking Request: " + this.getGeneratableEnergy() + " FE remaining, " + this.isOn);
+        if (this.getGeneratableEnergy() <= 0) {
+            this.charge();
         }
 
-        return new TickingRequest(TickRates.VibrationChamber, this.getRemainingFuelTicks() <= (double) 0.0F);
+        return new TickingRequest(TickRates.VibrationChamber, this.getGeneratableEnergy() <= 0);
     }
 
     public TickRateModulation tickingRequest(IGridNode node, int ticksSinceLastCall) {
-        System.out.println("Vibration Chamber Ticking: " + this.getRemainingFuelTicks() + " fuel ticks remaining, " + this.isOn);
-        this.minFuelTicksPerTick = this.minEnergyRate / this.getEnergyPerFuelTick();
-        this.maxFuelTicksPerTick = this.getMaxFuelTicksPerTick();
-        this.initialFuelTicksPerTick = this.initialEnergyRate / this.getEnergyPerFuelTick();
-        if (this.getRemainingFuelTicks() <= (double) 0.0F) {
-            this.eatFuel();
-            if (this.getRemainingFuelTicks() > (double) 0.0F) {
+        System.out.println("Singularity Generator Ticking: " + this.getGeneratableEnergy() + " FE remaining, " + this.isOn);
+        if (this.getGeneratableEnergy() <= 0) {
+            this.charge();
+            if (this.getGeneratableEnergy() > 0) {
                 return TickRateModulation.URGENT;
             } else {
-                this.setCurrentFuelTicksPerTick(this.initialFuelTicksPerTick);
                 return TickRateModulation.SLEEP;
             }
         } else {
-            double fuelTicksConsumed = (double) ticksSinceLastCall * this.currentFuelTicksPerTick;
-            this.setRemainingFuelTicks(this.getRemainingFuelTicks() - fuelTicksConsumed);
-            if (this.getRemainingFuelTicks() < (double) 0.0F) {
-                fuelTicksConsumed += this.getRemainingFuelTicks();
-                this.setRemainingFuelTicks(0.0F);
-            }
-
-            double speedScalingPerTick = (this.maxFuelTicksPerTick - this.minFuelTicksPerTick) / (double) 100.0F;
-            double speedStep = (double) ticksSinceLastCall * speedScalingPerTick;
-            IGrid grid = node.getGrid();
-            IEnergyService energy = grid.getEnergyService();
-            if (Math.abs(fuelTicksConsumed - (double) 0.0F) < 0.01) {
-                if (energy.injectPower(1.0F, Actionable.SIMULATE) == (double) 0.0F) {
-                    this.setCurrentFuelTicksPerTick(this.getCurrentFuelTicksPerTick() + speedStep);
-                    return TickRateModulation.FASTER;
-                } else {
-                    return TickRateModulation.IDLE;
-                }
-            } else {
-                double newPower = fuelTicksConsumed * this.getEnergyPerFuelTick();
-                long amount = this.inv.getStack(1) != null ? Objects.requireNonNull(this.inv.getStack(1)).amount() : 0L;
-                this.inv.setStack(1, new GenericStack(FluxKey.of(EnergyType.FE), (long) newPower + amount));
-                double overFlow = energy.injectPower(newPower, Actionable.MODULATE);
-                if (overFlow > (double) 0.0F) {
-                    this.setCurrentFuelTicksPerTick(this.getCurrentFuelTicksPerTick() - speedStep);
-                } else {
-                    this.setCurrentFuelTicksPerTick(this.getCurrentFuelTicksPerTick() + speedStep);
-                }
-
-                return overFlow > (double) 0.0F ? TickRateModulation.SLOWER : TickRateModulation.FASTER;
-            }
+            long newFE = (int) Math.min(ticksSinceLastCall * this.getGeneratePerTick(), this.getGeneratableEnergy());
+            long outputAmount = this.inv.getStack(1) != null ? Objects.requireNonNull(this.inv.getStack(1)).amount() : 0L;
+            long fixedNewFE = newFE + outputAmount > this.inv.getCapacity(ExternalTypes.FLUX) ? this.inv.getCapacity(ExternalTypes.FLUX) - outputAmount : newFE;
+            this.setGeneratableEnergy(this.getGeneratableEnergy() - fixedNewFE);
+            this.inv.setStack(1, new GenericStack(this.getEnergyKey(), fixedNewFE + outputAmount));
+            return fixedNewFE < newFE ? TickRateModulation.FASTER : TickRateModulation.SLOWER;
         }
     }
 
-    private void eatFuel() {
-        System.out.println("Vibration Chamber eating fuel: " + this.getRemainingFuelTicks() + " fuel ticks remaining, " + this.isOn);
+    private void charge() {
+        System.out.println("Singularity Generator charging: " + this.getGeneratableEnergy() + " FE remaining, " + this.isOn);
         GenericStack stack = this.inv.getStack(0);
-        System.out.println("Vibration Chamber fuel item: " + stack);
-        if (stack != null && stack.what().equals(AEItemKey.of(AEItems.SINGULARITY))) {
-            System.out.println("Vibration Chamber eating singularity fuel");
-            int newBurnTime = 100;
-            if (newBurnTime > 0 && stack.amount() > 0) {
-                this.setRemainingFuelTicks(this.getRemainingFuelTicks() + (double) newBurnTime);
-                this.setFuelItemFuelTicks(this.getRemainingFuelTicks());
+        System.out.println("Singularity Generator fuel item: " + stack);
+        if (stack != null && stack.what().equals(this.getFuelKey())) {
+            System.out.println("Singularity Generator charging singularity fuel");
+            if (stack.amount() > 0) {
+                this.setGeneratableEnergy(this.getGeneratableEnergy() + this.getEnergyPerSingularity());
                 if (stack.amount() <= 1) {
                     this.inv.setStack(0, GenericStack.fromItemStack(ItemStack.EMPTY));
                 } else {
@@ -222,54 +164,45 @@ public class SingularityGeneratorBlockEntity extends AENetworkedBlockEntity impl
             }
         }
 
-        if (this.getRemainingFuelTicks() > (double) 0.0F) {
+        if (this.getGeneratableEnergy() > 0) {
             this.getMainNode().ifPresent((grid, node) -> grid.getTickManager().wakeDevice(node));
         }
 
-        if (!this.isOn && this.getRemainingFuelTicks() > (double) 0.0F || this.isOn && this.getRemainingFuelTicks() <= (double) 0.0F) {
-            this.isOn = this.getRemainingFuelTicks() > (double) 0.0F;
+        if (!this.isOn && this.getGeneratableEnergy() > 0 || this.isOn && this.getGeneratableEnergy() <= 0) {
+            this.isOn = this.getGeneratableEnergy() > 0;
             this.markForUpdate();
             if (this.hasLevel()) {
                 Platform.notifyBlocksOfNeighbors(this.level, this.worldPosition);
             }
         }
-
     }
 
-    public double getCurrentFuelTicksPerTick() {
-        return this.currentFuelTicksPerTick;
+    public AEKey getEnergyKey() {
+        return FluxKey.of(EnergyType.FE);
     }
 
-    private void setCurrentFuelTicksPerTick(double currentFuelTicksPerTick) {
-        this.currentFuelTicksPerTick = Mth.clamp(currentFuelTicksPerTick, this.minFuelTicksPerTick, this.maxFuelTicksPerTick);
+    public AEKey getFuelKey() {
+        return AEItemKey.of(AEItems.SINGULARITY);
     }
 
-    public double getFuelItemFuelTicks() {
-        return this.fuelItemFuelTicks;
+    public long getEnergyCapacity() {
+        return 1048576; // FE
     }
 
-    private void setFuelItemFuelTicks(double fuelItemFuelTicks) {
-        this.fuelItemFuelTicks = fuelItemFuelTicks;
+    public int getGeneratePerTick() {
+        return 50; // FE per tick
     }
 
-    public double getRemainingFuelTicks() {
-        return this.remainingFuelTicks;
+    public int getEnergyPerSingularity() {
+        return 10000; // FE per singularity
     }
 
-    private void setRemainingFuelTicks(double remainingFuelTicks) {
-        this.remainingFuelTicks = remainingFuelTicks;
+    public double getGeneratableEnergy() {
+        return this.generatableEnergy;
     }
 
-    public double getEnergyPerFuelTick() {
-        return AEConfig.instance().getVibrationChamberBaseEnergyPerFuelTick();
-    }
-
-    public double getMaxFuelTicksPerTick() {
-        return this.getMaxEnergyRate() / this.getEnergyPerFuelTick();
-    }
-
-    public double getMaxEnergyRate() {
-        return this.baseMaxEnergyRate;
+    private void setGeneratableEnergy(double generatableEnergy) {
+        this.generatableEnergy = generatableEnergy;
     }
 
     @Override
