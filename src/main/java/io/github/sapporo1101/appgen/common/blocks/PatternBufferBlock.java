41212a479d55c4c0c100b3d16e1aca12e6830a89
sapporo1101 <71534849+sapporo1101@users.noreply.github.com>
2025-06-28 19:25:02 +0900

    add pattern buffer texture and localization

--- src/main/java/io/github/sapporo1101/appgen/common/blocks/PatternBufferBlock.java
+++ src/main/java/io/github/sapporo1101/appgen/common/blocks/PatternBufferBlock.java
@@ -8,7 +8,7 @@ import net.minecraft.world.entity.player.Player;
 
 public class PatternBufferBlock extends BlockBaseGui<PatternBufferBlockEntity> {
     public PatternBufferBlock() {
-        super(glassProps());
+        super(glassProps().noOcclusion());
     }
 
     @Override
