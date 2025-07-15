c0aa2d7d41cdcee3b5b508e0e262d3106405c201
sapporo1101 <71534849+sapporo1101@users.noreply.github.com>
2025-07-15 15:34:25 +0900

    change pattern_buffer and synthesizer textures

--- src/main/java/io/github/sapporo1101/appgen/recipe/GenesisSynthesizerRecipeBuilder.java
+++ src/main/java/io/github/sapporo1101/appgen/recipe/GenesisSynthesizerRecipeBuilder.java
@@ -19,6 +19,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+@SuppressWarnings("unused")
 public class GenesisSynthesizerRecipeBuilder {
     private final List<IngredientStack.Item> inputs = new ArrayList<>();
     private IngredientStack.Fluid fluid = null;
