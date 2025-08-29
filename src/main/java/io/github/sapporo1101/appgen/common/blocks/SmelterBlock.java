d95720df3926e84531f2378f0e3ab90da0e42c1d
sapporo1101 <71534849+sapporo1101@users.noreply.github.com>
2025-08-29 13:21:03 +0900

    add smelter light level and particles when smelting

--- src/main/java/io/github/sapporo1101/appgen/common/blocks/SmelterBlock.java
+++ src/main/java/io/github/sapporo1101/appgen/common/blocks/SmelterBlock.java
@@ -2,15 +2,24 @@ package io.github.sapporo1101.appgen.common.blocks;
 
 import appeng.api.orientation.IOrientationStrategy;
 import appeng.api.orientation.OrientationStrategies;
+import appeng.core.AEConfig;
 import appeng.menu.MenuOpener;
 import appeng.menu.locator.MenuLocators;
 import io.github.sapporo1101.appgen.common.blockentities.SmelterBlockEntity;
 import io.github.sapporo1101.appgen.menu.SmelterMenu;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nonnull;
 
@@ -19,7 +28,7 @@ public class SmelterBlock extends BlockBaseGui<SmelterBlockEntity> {
     public static final BooleanProperty WORKING = BooleanProperty.create("working");
 
     public SmelterBlock() {
-        super(metalProps());
+        super(metalProps().lightLevel(state -> state.getValue(WORKING) ? 13 : 0));
         this.registerDefaultState(this.defaultBlockState().setValue(WORKING, false));
     }
 
@@ -36,5 +45,30 @@ public class SmelterBlock extends BlockBaseGui<SmelterBlockEntity> {
     @Override
     public void openGui(SmelterBlockEntity tile, Player p) {
         MenuOpener.open(SmelterMenu.TYPE, p, MenuLocators.forBlockEntity(tile));
+    }
+
+    public void animateTick(@NotNull BlockState state, @NotNull Level level, @NotNull BlockPos pos, @NotNull RandomSource random) {
+        if (!AEConfig.instance().isEnableEffects()) return;
+
+        var be = this.getBlockEntity(level, pos);
+        if (be == null) return;
+
+        if (state.getValue(WORKING)) {
+            double d0 = pos.getX() + 0.5F;
+            double d1 = pos.getY();
+            double d2 = pos.getZ() + 0.5F;
+            if (random.nextDouble() < 0.1) {
+                level.playLocalSound(d0, d1, d2, SoundEvents.BLASTFURNACE_FIRE_CRACKLE, SoundSource.BLOCKS, 1.0F, 1.0F, false);
+            }
+
+            Direction direction = be.getFront();
+            Direction.Axis direction$axis = direction.getAxis();
+            double d3 = random.nextDouble() * 0.6 - 0.3;
+            double d4 = direction$axis == Direction.Axis.X ? direction.getStepX() * 0.52 : d3;
+            double d5 = random.nextDouble() * 9.0F / 16.0F;
+            double d6 = direction$axis == Direction.Axis.Z ? direction.getStepZ() * 0.52 : d3;
+            level.addParticle(ParticleTypes.SMOKE, d0 + d4, d1 + d5, d2 + d6, 0.0F, 0.0F, 0.0F);
+        }
+
     }
 }
